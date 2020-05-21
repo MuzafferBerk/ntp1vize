150a4157802c20c96ee93f243dc43cb064d8a3fa
Muzaffer Berk Duzgun <berkmdx@gmail.com>
2020-05-21 05:52:49 +0300

    Project Created.

---
+++ MuzafferBerkDuzgun183801021NTP1Vize/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MuzafferBerkDuzgun183801021NTP1Vize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Menu
+            //Add a book section
+            //View list of books section
+
+            //Book adding menu
+            //Ask the user how much books would like to add
+            //alinan deger ile dizi olustur. Dizi kitap sinifindan olusturgumuz dizi olsun
+
+            //Kullanicidan kitap bilgileri girilmesi istenecek
+            //kitap adi, yazar, basim tarihi, turu
+            //Kitap sinifi olustur bu bilgiler olsun icerisinde
+            //Goal : Kitap icin constructor ve encapsulation
+
+
+            //Kontroller :
+            //Basim yili 2020 > ise yeniden girilmesini iste
+            //Tum bilgiler duzgun girildikten sonra diziye kaydet kitabi
+
+            //Statik buffered reader with tray-catch
+            //Buffered writer metod in book class and static
+
+
+        }
+    }
+}
