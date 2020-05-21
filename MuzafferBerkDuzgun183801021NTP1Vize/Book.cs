5ca6ab487a49b720547f70f4173f63dee4b92299
Muzaffer Berk Duzgun <berkmdx@gmail.com>
2020-05-21 05:53:45 +0300

    Book class created.

---
+++ MuzafferBerkDuzgun183801021NTP1Vize/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MuzafferBerkDuzgun183801021NTP1Vize
+{
+    internal class Book
+    {
+        public string BookName { get; }
+        public string Author { get; }
+        public DateTime PublicationDate { get; }
+        public string Genre { get; }
+
+        #region Constructors
+
+        public Book(string bookName, string author, DateTime publicationDate, string genre)
+        {
+            BookName = bookName;
+            Author = author;
+            PublicationDate = publicationDate;
+            Genre = genre;
+        }
+
+        #endregion
+
+        internal static void SaveBook()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+    }
+}
